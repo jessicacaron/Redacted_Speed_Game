06bdf05f9a3e7785e708ff048c10f57d43b64e9f
Jaime Fuentes Mendieta <104949327+jaimemendieta@users.noreply.github.com>
2023-07-16 04:31:25 -0600

    Initial Card Setup
    A backend implementation of a standard 52 card deck. Demonstrate separating cards into different piles (in preparation for players hands, discard piles, game piles, etc.) Demonstrate displaying the cards in the console for debugging and verification purposes.

---
+++ server/deck.js
@@ -0,0 +1,34 @@
+const suits = ['SPADE', 'HEART', 'DIAMOND', 'CLUB'];
+const ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK', 'QUEEN', 'KING'];
+
+function createDeck() {
+    const deck = [];
+    for (const suit of suits) {
+        for (const rank of ranks) {
+            const card = { rank, suit };
+            deck.push(card);
+        }
+    }
+
+    console.log("Initial Deck");
+    console.log(deck); // Display deck to console for testing purposes
+
+    return deck;
+}
+
+//Knuth shuffle
+function shuffleDeck(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]]; // Swap cards at indices i and j
+    }
+    console.log("Shuffled Deck");
+    console.log(deck);
+}
+
+const deck = createDeck();
+shuffleDeck(deck);
+
+module.exports = deck; // exports shuffled deck
+module.exports.shuffleDeck = shuffleDeck; // exports the shuffle function
+
