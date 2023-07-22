7ccab8f0265fa3784da5bcbc652275a8d1f5fc17
Jaime Fuentes Mendieta <104949327+jaimemendieta@users.noreply.github.com>
2023-07-22 03:37:27 -0600

    Game rules for Classic Speed

--- backend/classicSpeedGame.js
+++ backend/classicSpeedGame.js
@@ -6,6 +6,10 @@ cd backend
 node classicSpeedGame.js
  */
 
+// Initialize hands for player1 and player2
+const player1Hand = [];
+const player2Hand = [];
+
 // Piles of cards
 const player1Pile = [];
 const player2Pile = [];
@@ -16,6 +20,10 @@ const centerRightPile = [];
 const discardedLeftPile = [];
 const discardedRightPile = [];
 const discardedUnionPile = []; // To join left and right discarded piles for shuffling
+
+// Variable to keep track of the number of cards drawn by each player
+let numCardsDrawnPlayer1 = 5;
+let numCardsDrawnPlayer2 = 5;
 
 
 // Deal cards to player1Pile and player2Pile
@@ -38,6 +46,9 @@ for (let i = 0; i < 6; i++) {
     centerRightPile.push(card2);
 }
 
+// Draw initial cards for each player's hand (assuming they have 5 cards in their pile to start)
+drawCards(player1Pile, player1Hand, 5);
+drawCards(player2Pile, player2Hand, 5);
 
 console.log("Player 1 Hand: ", player1Pile.length, " cards");
 console.log(player1Pile); // Display Player 1's pile in the console
@@ -50,3 +61,69 @@ console.log(centerLeftPile); // Display the left center pile in the console
 
 console.log("Center Right Pile: ", centerRightPile.length, " cards");
 console.log(centerRightPile); // Display the right center pile in the console
+
+
+// Function checks valid move on central pile
+function isValidMove(card, centralPile) {
+    if (centralPile.length === 0) {
+        return true; // If the central pile is empty, any card can be played
+    }
+
+    const topCard = centralPile[centralPile.length - 1];
+    const ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK', 'QUEEN', 'KING'];
+
+    // Convert rank to numerical value to perform comparisons
+    const cardRankValue = ranks.indexOf(card.rank);
+    const topCardRankValue = ranks.indexOf(topCard.rank);
+
+    // Check for special cases of playing "1" on "13" and vice versa
+    if ((cardRankValue === 0 && topCardRankValue === 12) || (cardRankValue === 12 && topCardRankValue === 0)) {
+        return true;
+    }
+
+    // Check if the card's rank is one higher or one lower than the top card on the central pile
+    return (
+        (cardRankValue === topCardRankValue + 1) ||
+        (cardRankValue === topCardRankValue - 1)
+    );
+}
+
+// Function to play a card from a player's pile onto the central pile
+function playCard(playerHand, centerLeftPile, centerRightPile, playedCard) {
+    const cardIndex = playerHand.findIndex((card) => card.id === playedCard.id);
+
+    if (playerHand.length === 0) {
+        return false; // If the player's hand is empty, they cannot play a card
+    }
+
+    if (cardIndex !== -1 && (isValidMove(playedCard, centerLeftPile) || isValidMove(playedCard, centerRightPile))) {
+        playerHand.splice(cardIndex, 1);
+
+        if (isValidMove(playedCard, centerLeftPile)) {
+            centerLeftPile.push(playedCard);
+        } else {
+            centerRightPile.push(playedCard);
+        }
+    }
+}
+
+
+// Function to draw cards from the player's pile and add them to their hand
+function drawCards(playerPile, hand) {
+    let cardsToDraw = 5 - hand.length;
+    while (hand.length < 5 && playerPile.length > 0 && cardsToDraw > 0) {
+        const card = playerPile.pop();
+        hand.push(card);
+        cardsToDraw--;
+    }
+}
+
+// Example usage:
+// Assuming player1 plays the first card in their pile to the centerLeftPile
+playCard(player1Pile, centerLeftPile, true);
+
+console.log("Player 1 Hand: ", player1Pile.length, " cards");
+console.log(player1Pile); // Updated player1Pile after playing the card
+
+console.log("Center Left Pile: ", centerLeftPile.length, " cards");
+console.log(centerLeftPile); // Updated centerLeftPile after the valid move
